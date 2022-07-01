a38238602b2c8d5b864db8e211c7d5f31788fad3
yusufnviiri <yusufnviiri@gmail.com>
2022-07-01 14:42:49 +0300

    Solve linter errors

--- src/__tests__/Rockets.test.js
+++ src/__tests__/Rockets.test.js
@@ -3,7 +3,7 @@ import renderer from 'react-test-renderer';
 import { render, screen } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import store from '../redux/configureStore';
-import Rockets from '../components/Rockets';
+import Rockets from '../redux/rockets/rockets';
 
 describe('Rocket Element Tests', () => {
   test('Check if Rockets Component Renders in DOM porperly', () => {
