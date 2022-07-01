19e4f70544f8a4067f2776d96008cb597a9c482f
yusufnviiri <yusufnviiri@gmail.com>
2022-07-01 08:22:02 +0300

    Solve git conflicts

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,8 +1,11 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import missionsReducer from './missions/missionsReducer';
+import rocketReducer from './rockets/rockets';
+
 
 const rootReducer = combineReducers({
   missions: missionsReducer,
+  rockets: rocketReducer,
 });
 
 const store = configureStore({reducer: rootReducer});
