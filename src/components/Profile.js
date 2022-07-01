9d321304bfa0c178ccd8daf3d784bbc1d6f9170f
yusufnviiri <yusufnviiri@gmail.com>
2022-07-01 07:29:03 +0300

    Edit Profiles file

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -2,13 +2,11 @@ import { useSelector } from 'react-redux';
 import ProfileMissionCard from './pages/ProfileMissionCard';
 
 const Profile = () => {
-  const rockets = useSelector((state) => state.rockets);
   const missions = useSelector((state) => state.missions);
   const myMissions = missions.filter((mission) => mission.reserved === true);
   //fetch reserved rockets from local storage
   let arr = localStorage.getItem("reserved");
   let arr1 = JSON.parse(arr);
-  console.log(arr1);
 
   return (
     <div>
