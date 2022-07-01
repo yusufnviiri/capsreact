9292134320acf54dfa362c5d0b8d079eae4ce045
yusufnviiri <yusufnviiri@gmail.com>
2022-07-01 12:12:13 +0300

    Revert "Solve linter errors"
    This reverts commit 08b5ba78e0198d1b9898a781874f2f2479aa975d.

--- src/redux/rockets/rockets.js
+++ src/redux/rockets/rockets.js
@@ -1,58 +1,74 @@
-const rocketUrl = 'https://api.spacexdata.com/v3/rockets';
+const rocketUrl = "https://api.spacexdata.com/v3/rockets";
 
-export const reserveRocket = (rocket) => ({
-  type: 'RESERVE_ROCKET',
-  payload: rocket,
-});
-
-export const cancelReservation = (rocket) => ({
-  type: 'CANCEL_RESERVATION',
-  payload: rocket,
-});
-
-export const fetchRockets = (rocketData) => ({
-  type: 'FETCH_ROCKETS',
-  payload: rocketData,
-});
-
-export const fetchRocketsAsync = () => (dispatch) => {
-  fetch(rocketUrl)
-    .then((response) => response.json())
-    .then((data) => dispatch(fetchRockets(data)))
-    .catch((error) => console.log(error));
+export const reserveRocket = (rocket) => {
+  return {
+    type: "RESERVE_ROCKET",
+    payload: rocket,
+  };
 };
-export const fetchReservedAsync = (rocketData) => (dispatch) => {
-  fetch(rocketUrl)
-    .then((response) => response.json())
-    .then(() => dispatch(reserveRocket(rocketData)))
-    .catch((error) => console.log(error));
-};
-export const cancelReservationAsync = (rocketData) => (dispatch) => {
-  fetch(rocketUrl)
 
-    .then((response) => response.json())
-    .then(() => dispatch(cancelReservation(rocketData)))
-    .catch((error) => console.log(error));
+export const cancelReservation = (rocket) => {
+    return {
+        type: "CANCEL_RESERVATION",
+        payload: rocket,
+    };
+}
+
+export const fetchRockets = (rocketData) => {
+  return {
+    type: "FETCH_ROCKETS",
+    payload: rocketData,
+  };
 };
+
+
+export const fetchRocketsAsync = () => {
+  return (dispatch) => {
+    fetch(rocketUrl)
+      .then((response) => response.json())
+      .then((data) => dispatch(fetchRockets(data)))
+      .catch((error) => console.log(error));
+  };
+};
+export const fetchReservedAsync = (rocketData) => {
+  return (dispatch) => {
+    fetch(rocketUrl)
+      .then((response) => response.json())
+      .then(() => dispatch(reserveRocket(rocketData)))
+      .catch((error) => console.log(error));
+  };
+};
+export const cancelReservationAsync = (rocketData) => {
+    return (dispatch) => {
+        fetch(rocketUrl)
+
+        .then((response) => response.json())
+        .then(() => dispatch(cancelReservation(rocketData)))
+        .catch((error) => console.log(error));
+    };
+}
+
 
 export const rocketReducer = (state = [], action) => {
   switch (action.type) {
-    case 'FETCH_ROCKETS':
+    case "FETCH_ROCKETS":
       return action.payload;
-    case 'RESERVE_ROCKET':
+    case "RESERVE_ROCKET":
       return state.map((rocket) => {
         if (rocket.id !== action.payload.id) {
           return rocket;
         }
         return { ...rocket, reserved: true };
       });
-    case 'CANCEL_RESERVATION':
-      return state.map((rocket) => {
-        if (rocket.id !== action.payload.id) {
-          return rocket;
+    case "CANCEL_RESERVATION":
+        return state.map((rocket) => {
+            if (rocket.id !== action.payload.id) {
+                return rocket;
+            }
+            return { ...rocket, reserved: false };
         }
-        return { ...rocket, reserved: false };
-      });
+        );
+
 
     default:
       return state;
