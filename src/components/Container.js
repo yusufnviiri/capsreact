92ff22de4d021efdd1a29b24453cf6155b03bd26
yusufnviiri <yusufnviiri@gmail.com>
2022-07-01 07:54:44 +0300

    Solve git conflicts

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,16 +1,18 @@
-import { Routes, Route } from 'react-router-dom';
-import Missions from './Missions';
-import NoMatch from './NoMatch';
-import Profile from './Profile';
+import { Routes, Route } from "react-router-dom";
+import Missions from "./Missions";
+import NoMatch from "./NoMatch";
+import Profile from "./Profile";
+import RocketsShow from "../redux/rockets/ShowRockets";
 
 function Wrapper() {
   return (
     <Routes>
+      <Route path="/" element={<RocketsShow />} />
       <Route path="/missions" element={<Missions />} />
       <Route path="/my-profile" element={<Profile />} />
       <Route path="/*" element={<NoMatch />} />
     </Routes>
   );
 }
-    
+
 export default Wrapper;
